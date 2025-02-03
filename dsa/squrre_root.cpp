3baf2fd2cbcd80cda6ac17dd7da457818fb54068
Mihir Chandra <chandramihir38@gmail.com>
2025-02-03 16:38:18 +0530

    correction

--- dsa/squrre_root.cpp
+++ dsa/squrre_root.cpp
@@ -5,11 +5,13 @@ int square(int k){
     int s=1;
     int e=k;
     int mid=0;
+    int ans=-1;
     while(s<=e){
         mid=s+(e-s)/2;
         if(mid*mid==k){
             return mid;
         }else if(mid*mid<k){
+            ans=mid;
             s=mid+1;
         }else{
             e=mid-1;
@@ -17,10 +19,24 @@ int square(int k){
     }
     return -1;
 }
+double precision(int n,int precise,int tempsol){
+    double ans=tempsol;
+    double factor=1;
+
+    for(int i=1;i<=3;i++){
+        factor=factor/10;
+        for(double j=ans;j*j<n;j=j+factor){
+            ans=j;
+        }
+
+    }
+    return ans;
+}
 
 int main(){
     int n=6;
-    cout<<square(n);
+    int tempsol=square(n);
+    cout<<precision(n,3,tempsol);
     
     return 0;
 }
