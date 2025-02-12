b94d7a4cef232d1ca942013fa6489565af0a4e60
Mihir Chandra <chandramihir38@gmail.com>
2025-02-12 12:21:41 +0530

    stringify

---
+++ dsa/char_strings.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    
+    char name[20];
+    string name1;
+    cin>>name;
+    
+    cin>>name1;
+
+    cout<<name<<endl;
+    cout<<name1<<endl;
+
+    return 0;
+}
