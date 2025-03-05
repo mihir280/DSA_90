6fcf24524fc5e7f98ed1a62209db90f123ae0d34
Mihir Chandra <chandramihir38@gmail.com>
2025-03-06 00:03:22 +0530

    topological_sort

---
+++ dsa/topological_sort.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<unordered_map>
+#include<list>
+#include<vector>
+#include<queue>
+using namespace std;
+
+int main(){
+
+    unordered_map<int,list<int>> adj;
+    int v;
+    vector<vector<int>> e;
+    for(int i=0;i<e.size();i++){
+        int u=e[i][0];
+        int v=e[i][1];
+
+        adj[u].push_back(v);
+    }
+
+    vector<int> indegree;
+    for(auto i:adj){
+        for(auto j:i.second){
+            indegree[j]++;
+        }
+    }
+
+    queue<int> q;
+    for(int i=0;i<v;i++){
+        if(indegree[i]==0){
+            q.push(i);
+        }
+    }
+    vector<int> ans;
+    while(!q.empty()){
+        int front=q.front();
+        q.pop();
+
+        ans.push_back(front);
+
+        for(auto i:adj[front]){
+            indegree[i]--;
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+
+
+    }
+
+
+    
+    return 0;
+}
